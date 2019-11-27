c9925294d9c865884f048375bbfb85f33a64fd03
Kuba <jmikulsk@wszib.edu.pl>
2019-11-27 22:08:59 +0100

    cos nie furga

--- src/jacobTodoList/Controller.java
+++ src/jacobTodoList/Controller.java
@@ -1,7 +1,11 @@
 package jacobTodoList;
 
+import jacobTodoList.datamodel.TodoData;
 import jacobTodoList.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
@@ -9,6 +13,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,39 +23,58 @@ public class Controller {
     private ListView<TodoItem> todoListView;
     @FXML
     private TextArea itemDetailsTextArea;
+    @FXML
+    private Label deadLineLabel;
 
     public void initialize(){
-        TodoItem item1= new TodoItem("Mail birthday card","Buy a 30th birthday card for John",
-                LocalDate.of(2019, Month.NOVEMBER,26));
-        TodoItem item2= new TodoItem("Doctors appointment","See dr. smith at 123 main street",
-                LocalDate.of(2019, Month.DECEMBER,6));
-        TodoItem item3= new TodoItem("Mikolaj quwa","Prezenty quwa",
-                LocalDate.of(2019, Month.DECEMBER,15));
-        TodoItem item4= new TodoItem("Dounat birthday ","Give a lot of love ",
-                LocalDate.of(2019, Month.NOVEMBER,28));
-        TodoItem item5= new TodoItem("Christmas  ","Snow Good and love ",
-                LocalDate.of(2019, Month.DECEMBER,24));
-        todoItems =  new ArrayList<TodoItem>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
-        todoListView.getItems().setAll(todoItems);
+//        TodoItem item1= new TodoItem("Mail birthday card","Buy a 30th birthday card for John",
+//                LocalDate.of(2019, Month.NOVEMBER,26));
+//        TodoItem item2= new TodoItem("Doctors appointment","See dr. smith at 123 main street",
+//                LocalDate.of(2019, Month.DECEMBER,6));
+//        TodoItem item3= new TodoItem("Mikolaj quwa","Prezenty quwa",
+//                LocalDate.of(2019, Month.DECEMBER,15));
+//        TodoItem item4= new TodoItem("Dounat birthday ","Give a lot of love ",
+//                LocalDate.of(2019, Month.NOVEMBER,28));
+//        TodoItem item5= new TodoItem("Christmas  ","Snow Good and love ",
+//                LocalDate.of(2019, Month.DECEMBER,24));
+//        todoItems =  new ArrayList<TodoItem>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//        TodoData.getInstance().setTodoItems(todoItems);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+                if(newValue!=null){
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("d MMMM  yyyy");//"d M yy");
+                    deadLineLabel.setText((df.format(item.getDeadline())));
+                }
+            }
+        });
+
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
 
     }
     @FXML
     public void handleClickListView()
     {
         TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadLineLabel.setText(item.getDeadline().toString());
 //        itemDetailsTextArea.setText();
 //        System.out.println(item);
-        StringBuilder sb = new StringBuilder(item.getDetails());
-          sb.append("\n\n\n\n");
-          sb.append("DUE: ");
-          sb.append(item.getDeadline().toString());
-        itemDetailsTextArea.setText(sb.toString());
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//          sb.append("\n\n\n\n");
+//          sb.append("DUE: ");
+//          sb.append(item.getDeadline().toString());
+//        itemDetailsTextArea.setText(sb.toString());
     }
 
 
--- src/jacobTodoList/Main.java
+++ src/jacobTodoList/Main.java
@@ -1,10 +1,13 @@
 package jacobTodoList;
 
+import jacobTodoList.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -16,8 +19,30 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    @Override
+    public void stop() throws Exception{
+        try{
+            TodoData.getInstance().storeTodoItems();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try{
+            TodoData.getInstance().loadTodoItems();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
